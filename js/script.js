46ebdb34b8b9acd7e8145c6bb7c05b3bd3aa387f
ORTIZ <aidenortiz@mail.ru>
2022-07-12 11:36:33 +0300

    Py - Добавлено приветствие.
    JS - Добавлено приветствие

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("Hello world");
+console.log("And welcome to my new project 'E-Wallet'.");
+console.log("Made by ORTIZ, 2022");
---
+++ python/django.py
@@ -0,0 +1,3 @@
+print("Hello, my friend")
+print("And welcome to my new project 'E-Wallet'.")
+print("Made by ORTIZ, 2022")
